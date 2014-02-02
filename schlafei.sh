c082d85d9ff13077dda449469c75afc790e5d896
wassi <der.wassi@web.de>
2014-02-02 12:27:06 +0100

    fix for init script

--- schlafei.sh
+++ schlafei.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+cd /home/pi/mean/
 #needed for pi-blaster
 gpio  -g mode 18 out
 gpio  -g mode 22 out
 gpio  -g mode 23 out
 gpio  -g mode 27 out
 
-node server.js
+/usr/local/bin/node server.js
+
 echo "27=0" > /dev/pi-blaster
 echo "18=0" > /dev/pi-blaster
 echo "22=0" > /dev/pi-blaster
